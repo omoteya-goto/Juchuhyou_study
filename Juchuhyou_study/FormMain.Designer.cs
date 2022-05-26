efbc3825f758323a86feef115b12fdd6a4a73a1b
GOTO-W10\goto <goto@omoteya.co.jp>
2022-05-26 10:30:42 +0900

    FitstCommit

---
+++ Juchuhyou_study/FormMain.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Juchuhyou_study
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.reportDataSet = new System.Data.DataSet();
+            this.JuchuReport = new System.Data.DataTable();
+            this.JuchuBango = new System.Data.DataColumn();
+            this.Juchubi = new System.Data.DataColumn();
+            this.Nouki = new System.Data.DataColumn();
+            this.Tokuisaki = new System.Data.DataColumn();
+            this.Nounyusaki = new System.Data.DataColumn();
+            this.Hinban = new System.Data.DataColumn();
+            this.Hinmei = new System.Data.DataColumn();
+            this.Suryo = new System.Data.DataColumn();
+            this.Tanka = new System.Data.DataColumn();
+            this.Kingaku = new System.Data.DataColumn();
+            this.Syukkabi = new System.Data.DataColumn();
+            this.GyoNo = new System.Data.DataColumn();
+            this.flexReport = new C1.Win.FlexReport.C1FlexReport();
+            this.tokuisakiName = new System.Windows.Forms.TextBox();
+            this.juchuhyouTableAdapter = new Juchuhyou_study.JuchuhyouDataSetTableAdapters.JuchuhyouMainTableAdapter();
+            this.juchuhyouDataSet = new Juchuhyou_study.JuchuhyouDataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.reportDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JuchuReport)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.juchuhyouDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Meiryo UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(16, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "得意先名";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Meiryo UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(24, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 35);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "受注表の印刷";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(432, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "検索";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // reportDataSet
+            // 
+            this.reportDataSet.DataSetName = "dataSet";
+            this.reportDataSet.Tables.AddRange(new System.Data.DataTable[] {
+            this.JuchuReport});
+            // 
+            // JuchuReport
+            // 
+            this.JuchuReport.Columns.AddRange(new System.Data.DataColumn[] {
+            this.JuchuBango,
+            this.Juchubi,
+            this.Nouki,
+            this.Tokuisaki,
+            this.Nounyusaki,
+            this.Hinban,
+            this.Hinmei,
+            this.Suryo,
+            this.Tanka,
+            this.Kingaku,
+            this.Syukkabi,
+            this.GyoNo});
+            this.JuchuReport.TableName = "JuchuReport";
+            // 
+            // JuchuBango
+            // 
+            this.JuchuBango.ColumnName = "JuchuBango";
+            // 
+            // Juchubi
+            // 
+            this.Juchubi.ColumnName = "Juchubi";
+            // 
+            // Nouki
+            // 
+            this.Nouki.ColumnName = "Nouki";
+            // 
+            // Tokuisaki
+            // 
+            this.Tokuisaki.ColumnName = "Tokuisaki";
+            // 
+            // Nounyusaki
+            // 
+            this.Nounyusaki.ColumnName = "Nounyusaki";
+            // 
+            // Hinban
+            // 
+            this.Hinban.ColumnName = "Hinban";
+            // 
+            // Hinmei
+            // 
+            this.Hinmei.ColumnName = "Hinmei";
+            // 
+            // Suryo
+            // 
+            this.Suryo.ColumnName = "Suryo";
+            // 
+            // Tanka
+            // 
+            this.Tanka.ColumnName = "Tanka";
+            // 
+            // Kingaku
+            // 
+            this.Kingaku.ColumnName = "Kingaku";
+            // 
+            // Syukkabi
+            // 
+            this.Syukkabi.ColumnName = "Syukkabi";
+            // 
+            // GyoNo
+            // 
+            this.GyoNo.ColumnName = "GyoNo";
+            // 
+            // flexReport
+            // 
+            this.flexReport.ReportDefinition = resources.GetString("flexReport.ReportDefinition");
+            this.flexReport.ReportName = "Juchuhyo";
+            // 
+            // tokuisakiName
+            // 
+            this.tokuisakiName.Location = new System.Drawing.Point(120, 80);
+            this.tokuisakiName.Name = "tokuisakiName";
+            this.tokuisakiName.Size = new System.Drawing.Size(280, 19);
+            this.tokuisakiName.TabIndex = 19;
+            // 
+            // juchuhyouTableAdapter
+            // 
+            this.juchuhyouTableAdapter.ClearBeforeFill = true;
+            // 
+            // juchuhyouDataSet
+            // 
+            this.juchuhyouDataSet.DataSetName = "JuchuhyouDataSet";
+            this.juchuhyouDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 143);
+            this.Controls.Add(this.tokuisakiName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.reportDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JuchuReport)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.juchuhyouDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Data.DataSet reportDataSet;
+        private C1.Win.FlexReport.C1FlexReport flexReport;
+        private System.Windows.Forms.TextBox tokuisakiName;
+        private System.Data.DataTable JuchuReport;
+        private System.Data.DataColumn JuchuBango;
+        private System.Data.DataColumn Juchubi;
+        private System.Data.DataColumn Nouki;
+        private System.Data.DataColumn Tokuisaki;
+        private System.Data.DataColumn Nounyusaki;
+        private System.Data.DataColumn Hinban;
+        private System.Data.DataColumn Hinmei;
+        private System.Data.DataColumn Suryo;
+        private System.Data.DataColumn Tanka;
+        private System.Data.DataColumn Kingaku;
+        private System.Data.DataColumn Syukkabi;
+        private JuchuhyouDataSetTableAdapters.JuchuhyouMainTableAdapter juchuhyouTableAdapter;
+        private System.Data.DataColumn GyoNo;
+        private JuchuhyouDataSet juchuhyouDataSet;
+    }
+}
+
---
+++ Juchuhyou_study/FormMain.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Juchuhyou_study
+{
+    public partial class FormMain : Form
+    {
+        SqlConnection connection = null;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// データの作成
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strTokuisakiName = tokuisakiName.Text;
+
+            //データベース接続
+            if (dBConnect(ref connection) == true)
+            {
+                //SqlDataAdapter sDAdapter = new SqlDataAdapter();
+
+                //SQL文
+                string strSql = "SELECT * FROM JuchuhyouMain where TokuisakiName = @tokuisakiName";
+
+                try
+                {
+
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = @strSql;
+                    command.Parameters.Add(new SqlParameter("@tokuisakiName", strTokuisakiName));
+                    //juchuhyouTableAdapter.Fill(juchuhyouDataSet.JuchuhyouMain);
+                    var adapter = new SqlDataAdapter(command);
+                    adapter.Fill(juchuhyouDataSet.JuchuhyouMain);
+                }
+                catch
+                {
+                    MessageBox.Show("データの取得に失敗しました。");
+                }
+                if (juchuhyouDataSet.JuchuhyouMain.Rows.Count == 0)
+                {
+                    MessageBox.Show("印刷する情報がありません", "情報", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                }
+                else if (juchuhyouDataSet.JuchuhyouMain.Rows.Count >= 1)
+                {
+                    //プレビュー
+                    C1.Win.FlexReport.C1FlexReport frReport = new C1.Win.FlexReport.C1FlexReport();
+                    C1.Win.FlexViewer.C1FlexViewerDialog svDialog = new C1.Win.FlexViewer.C1FlexViewerDialog();
+                    flexReport.Save("juchuhyouDataSet");
+                    frReport.Load("juchuhyouDataSet", "Juchuhyo");
+                    frReport.DataSource.Recordset = null;
+                    frReport.DataSource.Recordset = juchuhyouDataSet.JuchuhyouMain;
+                    svDialog.DocumentSource = frReport;
+                    svDialog.Show();
+                }
+
+            }
+        }
+        /// <summary>
+        /// データベースに接続
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        private Boolean dBConnect(ref SqlConnection connection)
+        {
+            //データベース接続文字列
+            string constr = @"Data Source=GOTO-W10\SQLEXPRESS01;Initial Catalog=Juchuhyou;Integrated Security=True";
+
+            connection = new SqlConnection(constr);
+
+            try
+            {
+                //データベースに接続
+                connection.Open();
+
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("データベースの接続に失敗しました。", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// string型に変換
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private string stringReturn(object value)
+        {
+            string strReturn = "";
+
+            if (value == null)
+            {
+                strReturn = string.Empty;
+            }
+            else
+            {
+                strReturn = value.ToString();
+            }
+
+            return strReturn;
+        }
+    }
+}
